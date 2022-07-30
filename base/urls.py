437a275456e44817e3645ce57117f5865839adf0
Aflah <72096386+aflah02@users.noreply.github.com>
2022-07-30 16:23:41 +0530

    Login Logout Done

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('logout/', views.logoutUser, name='logout'),
+    path('login/', views.loginPage, name='login'),
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
     path('create_room/', views.create_room, name='create_room'),
