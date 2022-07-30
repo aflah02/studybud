77cb56367c206b65fea05b340226eb1928bd1852
Aflah <72096386+aflah02@users.noreply.github.com>
2022-07-30 12:27:33 +0530

    Done Till Before CRUD

--- base/views.py
+++ base/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
+
+# rooms = [
+#     {'id':1, 'name':'Lets learn python'},
+#     {'id':2, 'name':'Design with me'},
+#     {'id':3, 'name':'Frontend developers'},
+# ]
 
 def home(request):
-    return HttpResponse("Home page")
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return HttpResponse("Room page")
-# Create your views here.
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
